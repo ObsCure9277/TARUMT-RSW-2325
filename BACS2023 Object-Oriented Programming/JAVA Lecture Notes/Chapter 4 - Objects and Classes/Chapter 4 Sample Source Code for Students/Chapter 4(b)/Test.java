4a6de396545d8a3600aab6b17ae251c7e276d27a
ShenZhi <ngshenzhi769@gmail.com>
2025-08-20 17:56:51 +0800

    Remove ignored folders from repository

--- BACS2023 Object-Oriented Programming/JAVA Lecture Notes/Chapter 4 - Objects and Classes/Chapter 4 Sample Source Code for Students/Chapter 4(b)/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-  public static void main(String[] args) {
-  	Student student = new Student();
-  	System.out.println("name? " + student.name);
-  	System.out.println("age? " + student.age);
-  	System.out.println("isScienceMajor? " + student.isScienceMajor);
-  	System.out.println("gender? " + student.gender);
-  }
-}
